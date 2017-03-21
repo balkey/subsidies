a3b58ee276c20a9d4f7ebb607b6f42b70cd21319
Balazs Krich <balkey80@gmail.com>
2017-03-21 06:58:35 +0100

    sqlite fix2

--- scripts/geocode_distinct2.py
+++ scripts/geocode_distinct2.py
@@ -47,7 +47,7 @@ def uniencoder(foo):
 	return boo
 
 API_KEY = ["AIzaSyB6x36wpibEnXKP8EoKu50vniEO3dEcNDo", "AIzaSyAXt42jURuoixnkHuoRHI7G-8gBznqMjkM", "AIzaSyDVctzdm79NIz3e0C1nEdrczNCYbi5Z4CE"]
-rounds = 2
+rounds = 0
 
 # USAGE LIKE: python geocode.py -i ../raw/clean_polski.csv -o ../raw/geocoded_polski.csv -r 2712
 ap = argparse.ArgumentParser()
@@ -80,69 +80,70 @@ for ids in beneficiaries:
 counter_a = 0
 counter_l = 0
 
-
-for company in beneficiaries:
-	if company not in mydict.keys():
-		PLACES_URL = "https://maps.googleapis.com/maps/api/place/textsearch/json?query="+company+" "+country+"&key="+API_KEY[rounds]
-		r = requests.get(PLACES_URL)
-		result_zero = json.loads(r.content)
-		print json.dumps(result_zero)
-		if result_zero["status"] == "OK" or result_zero["status"] == "ZERO_RESULTS":
-			if len(result_zero["results"]) > 0:
-				geocode_address(result_zero)
-				counter_a += 1
-				counter_l += 1
-				#writer.writerow(row[:11] + ["First", uniencoder(county), uniencoder(city_name), uniencoder(postal_code), lat_coords, long_coords])
-				#print county
-				#print city_name
-				#print postal_code
-				#print address
-				#print lat_coords
-				#print long_coords
-			else:
-				for i in beneficiaries[company]["variations"]:
-			 		PLACES_URL_v = "https://maps.googleapis.com/maps/api/place/textsearch/json?query="+i+" "+country+"&key="+API_KEY[rounds]
-			 		r_v = requests.get(PLACES_URL_v)
-					result_zero_v = json.loads(r_v.content)
-					if len(result_zero_v["results"]) > 0:
-						geocode_address(result_zero_v)
-						counter_a += 1
-						counter_l += 1
-						break
-					if beneficiaries[company]["variations"].index(i) == len(beneficiaries[company]["variations"]) - 1:
-						for i in beneficiaries[company]["variations"]:
-							PLACES_URL_v2 = "https://maps.googleapis.com/maps/api/place/textsearch/json?query="+i+"&key="+API_KEY[rounds]
-							r_v2 = requests.get(PLACES_URL_v2)
-							result_zero_v2 = json.loads(r_v2.content)
-							if len(result_zero_v2["results"]) > 0:
-								geocode_address(result_zero_v2)
-								counter_a += 1
-								counter_l += 1
-								break
-							if beneficiaries[company]["variations"].index(i) == len(beneficiaries[company]["variations"]) - 1:
-								county = "N/A"
-								city_name = "N/A"
-								postal_code = "N/A"
-								address = "N/A"
-								lat_coords = "N/A"
-								long_coords = "N/A"
-								mydict[company] = ["First", uniencoder(county), uniencoder(city_name), uniencoder(postal_code), uniencoder(address), lat_coords, long_coords]
-								counter_a += 1
-								counter_l += 1
-						
-		else:
-			if counter_l >= 1:
-				county = "N/A"
-				city_name = "N/A"
-				postal_code = "N/A"
-				address = "N/A"
-				lat_coords = "N/A"
-				long_coords = "N/A"
-				mydict[company] = ["First", uniencoder(county), uniencoder(city_name), uniencoder(postal_code), uniencoder(address), lat_coords, long_coords]
-				counter_l = 0
-			else:
-				if rounds < 2:
-					rounds += 1
+with SqliteDict('./my_db.sqlite') as mydict:
+	for company in beneficiaries:
+		if company not in mydict.keys():
+			PLACES_URL = "https://maps.googleapis.com/maps/api/place/textsearch/json?query="+company+" "+country+"&key="+API_KEY[rounds]
+			r = requests.get(PLACES_URL)
+			result_zero = json.loads(r.content)
+			print json.dumps(result_zero)
+			if result_zero["status"] == "OK" or result_zero["status"] == "ZERO_RESULTS":
+				if len(result_zero["results"]) > 0:
+					geocode_address(result_zero)
+					counter_a += 1
+					counter_l += 1
+					#writer.writerow(row[:11] + ["First", uniencoder(county), uniencoder(city_name), uniencoder(postal_code), lat_coords, long_coords])
+					#print county
+					#print city_name
+					#print postal_code
+					#print address
+					#print lat_coords
+					#print long_coords
 				else:
-					break
-		print counter_a
+					for i in beneficiaries[company]["variations"]:
+				 		PLACES_URL_v = "https://maps.googleapis.com/maps/api/place/textsearch/json?query="+i+" "+country+"&key="+API_KEY[rounds]
+				 		r_v = requests.get(PLACES_URL_v)
+						result_zero_v = json.loads(r_v.content)
+						if len(result_zero_v["results"]) > 0:
+							geocode_address(result_zero_v)
+							counter_a += 1
+							counter_l += 1
+							break
+						if beneficiaries[company]["variations"].index(i) == len(beneficiaries[company]["variations"]) - 1:
+							for i in beneficiaries[company]["variations"]:
+								PLACES_URL_v2 = "https://maps.googleapis.com/maps/api/place/textsearch/json?query="+i+"&key="+API_KEY[rounds]
+								r_v2 = requests.get(PLACES_URL_v2)
+								result_zero_v2 = json.loads(r_v2.content)
+								if len(result_zero_v2["results"]) > 0:
+									geocode_address(result_zero_v2)
+									counter_a += 1
+									counter_l += 1
+									break
+								if beneficiaries[company]["variations"].index(i) == len(beneficiaries[company]["variations"]) - 1:
+									county = "N/A"
+									city_name = "N/A"
+									postal_code = "N/A"
+									address = "N/A"
+									lat_coords = "N/A"
+									long_coords = "N/A"
+									mydict[company] = ["First", uniencoder(county), uniencoder(city_name), uniencoder(postal_code), uniencoder(address), lat_coords, long_coords]
+									counter_a += 1
+									counter_l += 1
+						
+			else:
+				if counter_l >= 1:
+					county = "N/A"
+					city_name = "N/A"
+					postal_code = "N/A"
+					address = "N/A"
+					lat_coords = "N/A"
+					long_coords = "N/A"
+					mydict[company] = ["First", uniencoder(county), uniencoder(city_name), uniencoder(postal_code), uniencoder(address), lat_coords, long_coords]
+					counter_l = 0
+				else:
+					if rounds < 2:
+						rounds += 1
+					else:
+						break
+			print counter_a
+			mydict.commit()
